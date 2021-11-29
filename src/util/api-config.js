b2035aebb9e6ca080042766313449b91df794f82
Chanakya <sunkarapallychanakya@gmail.com>
2021-11-29 20:57:46 +0530

    add env variable

--- src/util/api-config.js
+++ src/util/api-config.js
@@ -1,3 +1,2 @@
 export const baseURI = `${process.env.REACT_APP_HOST_URI}/api/v1`;
-export const netlifyProcessEnvURL = process.env.URL;
-console.log(process.env);
+export const netlifyProcessEnvURL = process.env.REACT_APP_PUBLIC_URL;
