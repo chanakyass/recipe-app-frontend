0efab30163b813e123690c797fb6b6c011658486
Chanakya <sunkarapallychanakya@gmail.com>
2023-10-23 16:58:05 +0530

    ft: updated yaml and collect unit test coverage

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import * as ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
-import reportWebVitals from './reportWebVitals';
 import "bootstrap/dist/css/bootstrap.min.css";
 import store from './store/setup';
 
@@ -19,4 +18,3 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
